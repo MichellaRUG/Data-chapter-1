085daabe1f6182fc25f9ccaa8669f8ac25c837a1
M. Ligtelijn <m.ligtelijn@rug.nl>
2025-06-02 16:56:37 +0200

    last version

--- DC1_part1_250602.R
+++ DC1_part1_250602.R
@@ -1,17 +1,19 @@
-#Hi Mo :)
+#Hi Mo :) how are you doing?
 
 
 rm(list=ls())
 
-usethis::use_git_config(user.name = "MichellaRUG", user.email = "m.ligtelijn@rug.nl")
+#usethis::use_git_config(user.name = "MichellaRUG", user.email = "m.ligtelijn@rug.nl")
 #usethis::create_github_token()     # Copy the token from browser
 #gitcreds::gitcreds_set()           # Paste the token in R
-usethis::use_git()                 # Initialize Git (if not yet done)
-usethis::use_github()              # Create & push repo to GitHub
+#usethis::use_git()                 # Initialize Git (if not yet done)
+#usethis::use_github()              # Create & push repo to GitHub
 
-#run the following 2 lines IN THE CONSOLE to update the newest version of the code to the GitHub website
-#system("git add .")
-#system("git push")
+#run the following 3 lines IN THE CONSOLE to update the newest version of the code to the GitHub website
+system("git add .")
+system('git commit -m "last version"')
+system("git push")
+
 
 
 #Load packages
